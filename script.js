f6e2488e6960a2656c819354ad42c44b139217b4
PixelQuinn <quinn.reams@icloud.com>
2023-05-23 19:06:47 -0400

    fix: fixed function and simplified code eliminating globals

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-// Assign values to rock, paper, and scissors
-let rock = 1;
-let paper = 2;
-let scissors = 3;
-
 // Function to get computer's choice
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-    
-    if (randomNumber === 1) {
-      return "rock";
-    } else if (randomNumber === 2) {
-      return "paper";
-    } else {
-      return "scissors";
-    }
-  }
+    const choices = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    return choices[randomNumber];
+}
+
 //rock paper and scissors
 
 // Test the function
-console.log(getComputerChoice(rock, scissors));
+console.log(getComputerChoice());
