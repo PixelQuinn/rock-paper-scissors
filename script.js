c3611517851125f97837ab7813d9242cfbc5bd78
PixelQuinn <quinn.reams@icloud.com>
2023-05-23 19:44:05 -0400

    feat: framework added to run the game in prompt windows

--- script.js
+++ script.js
@@ -34,3 +34,25 @@ function determineWinner(playerChoice, computerChoice) {
       return "Computer wins!";
     }
   }
+
+  // Function to play the game
+function playGame() {
+    while (true) {
+      const playerChoice = prompt("Enter your choice: rock, paper, or scissors").toLowerCase();
+  
+      if (!["rock", "paper", "scissors"].includes(playerChoice)) {
+        console.log("Invalid choice. Please try again.");
+        continue;
+      }
+  
+      const computerChoice = getComputerChoice();
+      console.log(`Player's choice: ${playerChoice}`);
+      console.log(`Computer's choice: ${computerChoice}`);
+      console.log(determineWinner(playerChoice, computerChoice));
+  
+      if (prompt("Do you want to play again? (yes/no)").toLowerCase() !== "yes") {
+        break;
+      }
+    }
+  }
+  
