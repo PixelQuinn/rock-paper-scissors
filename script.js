054cecfe4f46a4d6ffc260475bfcb89184b7e0a3
PixelQuinn <quinn.reams@icloud.com>
2023-05-24 12:50:38 -0400

    fix: removed dup code

--- script.js
+++ script.js
@@ -4,21 +4,6 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * choices.length);
     return choices[randomNumber];
   }
-  
-  // Function to determine the winner
-  function determineWinner(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-      return "It's a tie!";
-    } else if (
-      (playerChoice === "rock" && computerChoice === "scissors") ||
-      (playerChoice === "paper" && computerChoice === "rock") ||
-      (playerChoice === "scissors" && computerChoice === "paper")
-    ) {
-      return "You win!";
-    } else {
-      return "Computer wins!";
-    }
-  }
 
   // Function to determine the winner
 function determineWinner(playerChoice, computerChoice) {
